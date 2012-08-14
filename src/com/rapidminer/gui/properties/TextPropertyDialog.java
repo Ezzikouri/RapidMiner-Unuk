a96292bf975f1aba72c16ad7a1d2c71e9596bcea
rapid-i-admin <rapid-i-admin@d8dd750e-dc89-4b37-8fed-34212cb4890a>
2012-08-14 02:01:44 +0000

    Synchronization
    git-svn-id: https://rapidminer.svn.sourceforge.net/svnroot/rapidminer/Unuk@629 d8dd750e-dc89-4b37-8fed-34212cb4890a

--- src/com/rapidminer/gui/properties/TextPropertyDialog.java
+++ src/com/rapidminer/gui/properties/TextPropertyDialog.java
@@ -22,14 +22,24 @@
  */
 package com.rapidminer.gui.properties;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 import org.fife.ui.rsyntaxtextarea.RSyntaxDocument;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rtextarea.RTextScrollPane;
 
 import com.rapidminer.gui.properties.celleditors.value.TextValueCellEditor;
+import com.rapidminer.gui.tools.ResourceAction;
 import com.rapidminer.gui.tools.syntax.JEditTextArea;
 import com.rapidminer.operator.Operator;
 import com.rapidminer.parameter.ParameterTypeText;
+import com.rapidminer.tools.I18N;
 
 
 /**
@@ -49,16 +59,45 @@ public class TextPropertyDialog extends PropertyDialog {
 
     private RSyntaxTextArea textArea = new RSyntaxTextArea();
     
+    private JButton resize = new JButton();
+    
     public TextPropertyDialog(final ParameterTypeText type, String text, Operator operator) {
         super(type, "text");
         this.text = text;
+        ResourceAction resourceAction = new ResourceAction(true, "text_dialog.enlarge") {
+			
+			private static final long serialVersionUID = 8857840715142145951L;
 
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+				Dimension dim = new Dimension( (int)(screenDim.width * 0.9),(int)(screenDim.height* 0.9));
+				Dimension normal = getDefaultSize(NORMAL);
+				Dimension currentSize = getSize();
+				if (currentSize.getHeight() != dim.getHeight() && currentSize.getWidth() != dim.getWidth()){
+					setSize(dim);
+					setLocationRelativeTo(null);
+					resize.setText(I18N.getGUIBundle().getString("gui.action.text_dialog.shrink.label"));
+					resize.setToolTipText(I18N.getGUIBundle().getString("gui.action.text_dialog.shrink.tip"));
+					resize.setMnemonic(I18N.getGUIBundle().getString("gui.action.text_dialog.shrink.mne").charAt(0));
+				}else{
+					setSize(normal);
+					setDefaultLocation();
+					resize.setText(I18N.getGUIBundle().getString("gui.action.text_dialog.enlarge.label"));
+					resize.setToolTipText(I18N.getGUIBundle().getString("gui.action.text_dialog.enlarge.tip"));
+					resize.setMnemonic(I18N.getGUIBundle().getString("gui.action.text_dialog.enlarge.mne").charAt(0));
+				}
+			}
+		};
+		
+        resize = new JButton(resourceAction);
+        
         textArea.setDocument(new RSyntaxDocument(type.getTextType().getSyntaxIdentifier()));
         textArea.setText(text);
         textArea.setAnimateBracketMatching(type.getTextType().isBracketMatching());
         textArea.setAutoIndentEnabled(type.getTextType().isAutoIntending());
         textArea.setAutoscrolls(true);
-        layoutDefault(new RTextScrollPane(textArea), NORMAL, makeOkButton(), makeCancelButton());
+        layoutDefault(new RTextScrollPane(textArea), NORMAL, resize, makeOkButton(), makeCancelButton());
         
         textArea.requestFocusInWindow();
     }
